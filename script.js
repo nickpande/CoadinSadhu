eec28314c8c8d0141a69b4e610564a936a39b966
nickpande <86617735+nickpande@users.noreply.github.com>
2024-04-04 09:55:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav(){
+    console.log("hll");
+    document.getElementById("myNave").style.width="100%";
+}
+function closeNav(){
+    document.getElementById("myNave").style.width="0%";
+}
+
